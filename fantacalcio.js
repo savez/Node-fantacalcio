e1328baa4d82c1b5df0c295616ef4fc45742a279
saverio menin <saverio@link-up.it>
2017-09-18 18:56:52 +0200

    version 1.0.1

--- fantacalcio.js
+++ fantacalcio.js
@@ -52,14 +52,21 @@ function buildObj(html){
         let element = classi.substring(20);
         formazioni[index].update = $('div.'+element+' span.lastUpdate').text();
         formazioni[index].partita = $('div.'+element+' div.matchFieldInner div.matchDateTime').text();
-        listClass.forEach((elem) => {
+        listClass.forEach((elem,index2) => {
             foo = buildObjSquadra();
             foo.allenatore = $('div.'+element+' div.matchFieldInner div.'+elem+'Module span.mister').text();
             foo.nome = $('div.'+element+' div.'+elem+'Team span.teamName a').text();
             foo.modulo = $('div.'+element+' div.matchFieldInner div.'+elem+'Module span.modulo').text();
 
             // @Todo: Giocatori
+            let giocatori = null;
+            if(index2 <= 0)
+                giocatori = $('div.'+element+' div.matchFieldInner ul.team-players-container li').first().html();
+            else
+                giocatori = $('div.'+element+' div.matchFieldInner ul.team-players-container li').last().html();
 
+            const $giocatori = cheerio.load(giocatori);
+            foo.giocatori = $giocatori('.team-players').html();
             formazioni[index][elem] = foo;
         });
     });
